aaa27da6c7d8e67402fd5d3fb293cbbfb66ad5c0
byron--sama <albyron.10.z@gmail.com>
2019-06-20 20:06:39 -0600

    Hello World Consola Main

---
+++ HelloWorldConsola/src/helloworldconsola/HelloWorldConsola.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworldconsola;
+
+/**
+ *
+ * @author Byron Murillo
+ */
+
+public class HelloWorldConsola {
+
+    /**
+     * @param args the command line arguments
+     */
+    private static void ImprimirMensaje() {
+        System.out.println("Hello Gaby !!!");
+    }
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        ImprimirMensaje();
+    }//Fin de main
+
+}
